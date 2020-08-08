e511d411fc20595b94807e256db2890862b89c8b
Moyses Aguagallo <leo.aigaje12@gmail.com>
2020-08-08 10:51:53 -0500

    Optimizacion de tiempo en ejecucion

--- mainbot.py
+++ mainbot.py
@@ -8,56 +8,68 @@ import json
 import random
 import pickle
 
-#nltk.download('punkt') #errores al compilar
+#nltk.download('punkt') #al inicio de la compilacion
 
+#instanciacion de la clase
 stemmer = LancasterStemmer()
 
 # extracion de datos del json
-with open('question.json') as questions:
+with open('question.json', encoding = 'utf-8') as questions:
     data = json.load(questions)
 
-words = []
-tags = []
-aux_x = [] # aux_word
-aux_y = [] # aux_tags
+try:
+    # si existe datos de entrenamiento abrir en modo lectura
+    with open('trainig_data.pickle', 'rb') as pickle_file:
+        #inicializar variables con datos
+        words, tags, training, out = pickle.load(pickle_file)
+except:
 
-# recorre los datos del json
-for q in data["question"]:
-    for clue in q["patrones"]:
-        auxWord = nltk.word_tokenize(clue)  #separa palabras
+    words = []
+    tags = []
+    aux_x = [] # aux_word
+    aux_y = [] # aux_tags
 
-        words.extend(auxWord)  #agregamos todas las palabras
-        aux_x.append(auxWord) #agregamos la frase
-        aux_y.append(q["tag"]) #agregamos el tag
+    # recorre los datos del json
+    for q in data["question"]:
+        for clue in q["patrones"]:
+            auxWord = nltk.word_tokenize(clue)  #separa palabras
 
-        if q["tag"] not in tags: #no se repitan el tag
-            tags.append(q["tag"])
+            words.extend(auxWord)  #agregamos todas las palabras
+            aux_x.append(auxWord) #agregamos la frase
+            aux_y.append(q["tag"]) #agregamos el tag
 
-words = [stemmer.stem(w.lower()) for w in words if w != "?"] #conversion de palabras a minusculas
-words = sorted(list(set(words))) # ordenar palabras
-tags = sorted(tags)
+            if q["tag"] not in tags: #no se repitan el tag
+                tags.append(q["tag"])
 
-training = []
-out = []
-empty_out = [ 0 for _ in range(len(tags))] # salida vacia oon diemension de tags
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"] #conversion de palabras a minusculas
+    words = sorted(list(set(words))) # ordenar palabras
+    tags = sorted(tags)
 
-for indice, objectWord in enumerate(aux_x):
-    cubeta = []
-    aux_wd = [stemmer.stem(w.lower()) for w in objectWord]
+    training = []
+    out = []
+    empty_out = [ 0 for _ in range(len(tags))] # salida vacia oon diemension de tags
 
-    for w in words:
-        if w in aux_wd:
-            cubeta.append(1)
-        else:
-            cubeta.append(0)
+    for indice, objectWord in enumerate(aux_x):
+        cubeta = []
+        aux_wd = [stemmer.stem(w.lower()) for w in objectWord]
 
-    row_out = empty_out[:]
-    row_out[tags.index(aux_y[indice])] = 1
-    training.append(cubeta)
-    out.append(row_out)
+        for w in words:
+            if w in aux_wd:
+                cubeta.append(1)
+            else:
+                cubeta.append(0)
 
-training = numpy.array(training) # conversion a arreglos numpy
-out = numpy.array(out)
+        row_out = empty_out[:]
+        row_out[tags.index(aux_y[indice])] = 1
+        training.append(cubeta)
+        out.append(row_out)
+
+    training = numpy.array(training) # conversion a arreglos numpy
+    out = numpy.array(out)
+
+    # guardar datos de entrenamiento en modo escritura
+    with open('trainig_data.pickle', 'wb') as pickle_file:
+        pickle.dump((words, tags, training, out), pickle_file)
 
 tensorflow.reset_default_graph() # reseteo de red
 
@@ -68,8 +80,15 @@ net = tflearn.fully_connected(net, len(out[0]), activation = "softmax") #capa de
 net = tflearn.regression(net) #algoritmo de entrenamiento
 
 model = tflearn.DNN(net)
-model.fit(training, out, n_epoch = 100, batch_size = 10, show_metric = True)
-model.save("model.tflearn")
+
+try:
+    #si existe el modelo lo cargamos
+    model.load('model.tflearn')
+except:
+    #entrenar el modelo
+    model.fit(training, out, n_epoch = 100, batch_size = 10, show_metric = True)
+    #guardar el modelo
+    model.save("model.tflearn")
 
 def mainBot():
 
